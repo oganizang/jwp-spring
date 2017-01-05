83b0658e9a3b4bdf97fe1a31b339b57e55c7b28d
oganizang <jangow77@gmail.com>
2017-01-05 22:58:20 +0900

    AppConfig edit

--- src/main/java/next/config/AppConfig.java
+++ src/main/java/next/config/AppConfig.java
@@ -1,10 +1,25 @@
 package next.config;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = { "next.dao", "next.service" })
 public class AppConfig {
+	private static final String DB_DRIVER = "org.h2.Driver";
+	private static final String DB_URL = "jdbc:h2:~/jwp-basic;AUTO_SERVER=TRUE";
+	private static final String DB_USERNAME = "sa";
+	private static final String DB_PW = "";
 	
+	public static DataSource getDataSource() {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName(DB_DRIVER);
+		ds.setUrl(DB_URL);
+		ds.setUsername(DB_USERNAME);
+		ds.setPassword(DB_PW);
+		return ds;
+	}
 }
